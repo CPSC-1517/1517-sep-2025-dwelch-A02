788ca26415752d754439476924f664a8eed7ea98
Don Welch <dwelch@nait.ca>
2025-09-17 11:44:48 -0600

    complete constructor unit tests; valid and invalid, demo Theory annotation test, Demo exception testing

--- OOPsSolution/OOPsReview/Person.cs
+++ OOPsSolution/OOPsReview/Person.cs
@@ -11,12 +11,35 @@ namespace OOPsReview
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public ResidentAddress Address { get; set; }
-        public List<Employment> EmploymentPositions { get; set; }
+
+        //when an instance is being created, the o/s will first
+        //  generate your storage areas, assign default datatype values,
+        //  assign declaration assignment values THEN execute the appropriate constructor
+        public List<Employment> EmploymentPositions { get; set; } = new List<Employment>();
         public Person()
         {
             FirstName = "Unknown";
             LastName = "Unknown";
-
+            //the following line was removed when Refactoring after the greedy constructor test
+            //EmploymentPositions = new List<Employment>();
+        }
+        public Person(string firstname, string lastname,
+                    ResidentAddress address, List<Employment> employments)
+        {
+            if (string.IsNullOrWhiteSpace(firstname))
+                throw new ArgumentNullException("FirstName","First name cannot be missing or blank");
+            if (string.IsNullOrWhiteSpace(lastname))
+                throw new ArgumentNullException("LastName", "Last name cannot be missing or blank");
+            FirstName = firstname.Trim();
+            LastName = lastname.Trim();
+            Address = address;
+            if (employments != null)
+                //save the list sent in
+                EmploymentPositions = employments;
+            //the following line was removed when Refactoring after the greedy constructor test
+            //else
+            //    //parameter has not list instance
+            //    EmploymentPositions = new List<Employment>();
         }
     }
 }
--- OOPsSolution/TDDUnitTesting/Person_Should.cs
+++ OOPsSolution/TDDUnitTesting/Person_Should.cs
@@ -13,7 +13,6 @@ namespace TDDUnitTesting
     {
         #region Constructors
         #region for valid data
-        #endregion
         //a Fact unit test executes once
         //without the [Fact] annotation, the method is NOT considered a unit test
         //  it would just be a method within this class
@@ -37,7 +36,163 @@ namespace TDDUnitTesting
             sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionCount);
         }
 
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_With_No_Address_Or_Employments()
+        {
+            //Arrange 
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            int expectedEmploymentPositionCount = 0;
+
+            //Act 
+            Person sut = new Person("  Don ","   Welch  ",null,null);
+
+            //Assert 
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionCount);
+        }
+
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_With_No_Employments()
+        {
+            //Arrange 
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            ResidentAddress expectedAddress = new ResidentAddress(123, "Maple St.",
+                                "Edmonton", "AB", "T6Y7U8");
+            int expectedEmploymentPositionCount = 0;
+
+            //Act 
+            Person sut = new Person("  Don ", "   Welch  ", expectedAddress, null);
+
+            //Assert 
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().Be(expectedAddress);
+            sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionCount);
+        }
+
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_With_All_Data()
+        {
+            //Arrange 
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            ResidentAddress expectedAddress = new ResidentAddress(123, "Maple St.",
+                                "Edmonton", "AB", "T6Y7U8");
+
+            //how to test a collection?
+            //create individual instances of the item in the list
+            //in this example those instances are objects
+            //you must remember each object has a unique GUID
+            //NOTE: you CANNOT reuse a single variable to hold the separate instances
+            Employment one = new Employment("PG I", SupervisoryLevel.TeamMember,
+                                        DateTime.Parse("2013/10/10"), 6.5);
+            //remember that if no year was supplied the length of holding the current
+            //      position is calculated for the instance
+            Employment two = new Employment("PG II", SupervisoryLevel.TeamLeader,
+                                DateTime.Parse("2020/10/10"));
+            List<Employment> employments = new List<Employment>();
+            employments.Add(one);
+            employments.Add(two);
+            int expectedEmploymentPositionCount = 2;
+
+            //Act 
+            Person sut = new Person("  Don ", "   Welch  ", expectedAddress, employments);
+
+            //Assert 
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().Be(expectedAddress);
+            //best practise to check the count of your list before checking the contents
+            sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionCount);
+            //what about the collection itself
+            // - it has to have the correct instances
+            // - the instances should be in the same physical order
+            sut.EmploymentPositions.Should().ContainInConsecutiveOrder(employments);
+        }
+        #endregion
+
+
         #region for exception testing
+        //the second test annotation used is called [Theory]
+        //it will execute n number of times as a loop
+        //n is determined by the number [InlineData()] annotations following the [Theory]
+        //to setup the test header, you must include a parameter in a parameter list
+        //  one for each, value in the InlineData set of values
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        public void Throw_Exception_Creating_Instance_Using_Greedy_Constructor_For_Missing_FirstName(string firstname)
+        {
+            //Arrange
+            //possible values for FirstName: null, empty string, blank string
+            //the setup of an exception test does not have to be as extensive as a successful test
+            //  as the objective is to catch the exception that is thrown
+            //in this example there will be no need to check expected values
+
+            //Act
+            //the act in this case is the capture of the exception that has been thrown
+            //use () => to indicate that the following delegate is to be executed as the required code
+            Action action = () => new Person(firstname, "Welch", null, null);
+
+            //Assert
+            //test to see if the expected exception was throw
+            action.Should().Throw<ArgumentNullException>();
+        }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        public void Throw_Exception_Creating_Instance_Using_Greedy_Constructor_For_Missing_LastName(string lastname)
+        {
+            //Arrange
+            //possible values for FirstName: null, empty string, blank string
+            //the setup of an exception test does not have to be as extensive as a successful test
+            //  as the objective is to catch the exception that is thrown
+            //in this example there will be no need to check expected values
+
+            //Act
+            //the act in this case is the capture of the exception that has been thrown
+            //use () => to indicate that the following delegate is to be executed as the required code
+            Action action = () => new Person("Don", lastname, null, null);
+
+            //Assert
+            //test to see if the expected exception was throw
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        //combine the two previous Theory tests
+        //multiple values in InlinData
+        //multiple parameters on your method
+        [Theory]
+        
+        [InlineData(null,"Welch")]
+        [InlineData("","Welch")]
+        [InlineData("    ","Welch")]
+        [InlineData("Don",null)]
+        [InlineData("Don","")]
+        [InlineData("Don","    ")]
+        public void Throw_Exception_Creating_Instance_Using_Greedy_Constructor_For_Missing_First_Or_Last_Name(string firstname,string lastname)
+        {
+            //Arrange
+            //possible values for FirstName: null, empty string, blank string
+            //the setup of an exception test does not have to be as extensive as a successful test
+            //  as the objective is to catch the exception that is thrown
+            //in this example there will be no need to check expected values
+
+            //Act
+            //the act in this case is the capture of the exception that has been thrown
+            //use () => to indicate that the following delegate is to be executed as the required code
+            Action action = () => new Person(firstname, lastname, null, null);
+
+            //Assert
+            //test to see if the expected exception was throw
+            action.Should().Throw<ArgumentNullException>();
+        }
         #endregion
         #endregion
 
