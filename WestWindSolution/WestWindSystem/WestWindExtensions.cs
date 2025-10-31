1fd4850c890133e4a4c776d3d8d3c7099eeeabc4
Don Welch <dwelch@nait.ca>
2025-10-31 14:23:51 -0600

    build a service class and method:BuildVersion, register service class, code access to services in Program.cs, consume service on home page

--- WestWindSolution/WestWindSystem/WestWindExtensions.cs
+++ WestWindSolution/WestWindSystem/WestWindExtensions.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WestWindSystem.BLL;
 using WestWindSystem.DAL;
 #endregion
 
@@ -33,6 +34,25 @@ namespace WestWindSystem
             //we wish it to use the string I send in on each creation of the
             //      instance of my DbContext class
             services.AddDbContext<WestWindContext>(options);
+
+            //to register a service class you will use the IServiceCollection method
+            //  .AddTransient<T> where T is the service class name
+            //for any outside user coding that requires access to one or more services
+            //  you MUST register the service class, hence all public methods within the class
+            //  will be available
+            services.AddTransient<BuildVersionServices>((serviceProvider) =>
+            {
+                //obtain the context information already registered in
+                //      IServiceCollection (see above)
+                var context = serviceProvider.GetService<WestWindContext>();
+
+                //create an instance of the services class when the service is requested
+                //once the class has been registered, it can be used by ANY outside
+                //  source as long as the outside source has referenced the extension 
+                //  class method (see your Program.cs in your wb app)
+                return new BuildVersionServices(context);
+            });
+           
         }
     }
 }
