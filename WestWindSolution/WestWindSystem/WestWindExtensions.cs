637db5d34ab7de670fc87c62922627f84bdeb416
Don Welch <dwelch@nait.ca>
2025-11-26 11:50:30 -0700

    complete clear and create, form, form events, service methods,custom validation on web form and service events

--- WestWindSolution/WestWindSystem/WestWindExtensions.cs
+++ WestWindSolution/WestWindSystem/WestWindExtensions.cs
@@ -78,6 +78,11 @@ namespace WestWindSystem
                 var context = serviceProvider.GetService<WestWindContext>();
                 return new SupplierServices(context);
             });
+            services.AddTransient<ProductServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+                return new ProductServices(context);
+            });
         }
     }
 }
